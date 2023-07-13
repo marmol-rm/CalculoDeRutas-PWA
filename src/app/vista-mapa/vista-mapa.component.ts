ee1568ba11a9338cde3de25ebc1d99b2c4142ece
Marcelo Molina <34018240+marmol-rm@users.noreply.github.com>
2023-07-12 21:07:51 -0600

    Probando trazado de rutas

--- src/app/vista-mapa/vista-mapa.component.ts
+++ src/app/vista-mapa/vista-mapa.component.ts
@@ -1,10 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {LeafletService} from "../service/leaflet.service";
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 
-
-export const DEFAULT_LAT = 48.20807;
-export const DEFAULT_LON =  16.37320;
-export const TITULO = 'Proyecto';
+export const TITULO = 'Cálculo de Rutas';
 var iconRetinaUrl = 'assets/marker-icon-2x.png';
 var iconUrl = 'assets/marker-icon.png';
 var shadowUrl = 'assets/marker-shadow.png';
@@ -16,77 +14,56 @@ var shadowUrl = 'assets/marker-shadow.png';
 })
 export class VistaMapaComponent implements OnInit {
 
-  title = 'Mapa';
-
-  private map:any;
-  @Input() lat: number = DEFAULT_LAT;
-  @Input() lon: number = DEFAULT_LON;
-  @Input() titulo: string = TITULO ;
-
-  constructor(private mapService: LeafletService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    if (this.mapService.L) {
-      this.initMap();
+    var map: L.Map;
+    navigator.geolocation.watchPosition(exito, error);
+
+    function exito(pos: { coords: { latitude: number; longitude: number; accuracy: number; }; }) {
+      const ltt = pos.coords.latitude;
+      const lgt = pos.coords.longitude;
+      const acu = pos.coords.accuracy;
+
+      var iconDefault = L.icon({
+        iconRetinaUrl,
+        iconUrl,
+        shadowUrl,
+        iconSize: [35, 55],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowSize: [41, 41]
+      });
+      L.Marker.prototype.options.icon = iconDefault;
+
+      map = L.map('map').setView([ltt, lgt], 17);
+      var marker = L.marker([ltt, lgt]).addTo(map);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+
+      map.on('click', function (e) {
+        var mark = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+
+        L.Routing.control({
+          waypoints: [
+            L.latLng(ltt,lgt),
+            L.latLng(e.latlng.lat, e.latlng.lng)
+          ]
+        }).on('routesfound', function (e) {
+          console.log(e);
+        }).addTo(map);
+      });
+
     }
-  }
 
-  private initMap(): void {
-    var iconDefault = this.mapService.L.icon({
-      iconRetinaUrl,
-      iconUrl,
-      shadowUrl,
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      tooltipAnchor: [16, -28],
-      shadowSize: [41, 41]
-    });
-    this.mapService.L.Marker.prototype.options.icon = iconDefault;
+    function error() {
 
-    this.map =  this.mapService.L.map('map', {
-      center: [this.lat, this.lon],
-      attributionControl: false,
-      zoom: 14
-    });
-
-    const tiles = this.mapService.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://1938.com.es">Web Inteligencia Artificial</a>'
-    });
-    //const marker = L.marker([this.lat, this.lon]);
-    //marker.addTo(this.map);
-
-    const lon = this.lon + 0.009;
-    const lat = this.lat + 0.009;
-    const marker = this.mapService.L.marker([lat + 0.005, lon + 0.005]).bindPopup(this.titulo);
-    marker.addTo(this.map);
-
-    const mark = this.mapService.L.circleMarker([this.lat, this.lon]).addTo(this.map);
-    mark.bindPopup(this.titulo);
-    mark.addTo(this.map);
-
-    const mark2 = this.mapService.L.circleMarker([lat, lon]).addTo(this.map);
-    mark2.addTo(this.map);
-
-
-
-    this.mapService.L.Routing.control({
-      router: this.mapService.L.Routing.osrmv1({
-        serviceUrl: `https://router.project-osrm.org/route/v1/`
-      }),
-      showAlternatives: true,
-      fitSelectedRoutes: false,
-      show: false,
-      routeWhileDragging: true,
-      waypoints: [
-        this.mapService.L.latLng(this.lat, this.lon),
-        this.mapService.L.latLng(lat, lon)
-      ]
-    }).addTo(this.map);
-
-    tiles.addTo(this.map);
+    }
   }
 
 }
